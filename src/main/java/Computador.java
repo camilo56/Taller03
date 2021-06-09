fdac6cc39852d82be623d29ca7f45e84a9e4dacd
camilo56 <76082597+camilo56@users.noreply.github.com>
2021-06-09 10:02:42 -0400

    Creacion de la clase Computador y getter and setter
    Signed-off-by: camilo56 <76082597+camilo56@users.noreply.github.com>

---
+++ src/main/java/Computador.java
@@ -0,0 +1,32 @@
+public class Computador {
+    private String marca;
+    private String modelo;
+    private Ram ram1;
+
+    public Computador() {
+        this.marca = "Gear";
+        this.modelo = "x-300";
+
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+
+    }
+
+
+
+}
