2c319b8e0fcd57e4644ee0c3cf1f82a2cf4f4a7c
camilo56 <76082597+camilo56@users.noreply.github.com>
2021-06-09 09:33:02 -0400

    Creacion de la clase Ram
    Signed-off-by: camilo56 <76082597+camilo56@users.noreply.github.com>

---
+++ src/main/java/Ram.java
@@ -0,0 +1,44 @@
+public class Ram {
+    private String marca="";
+    private int capacidad=0;
+    private int frecuancia=0;
+
+    public Ram() {
+        this.frecuancia = frecuancia;
+        this.marca = marca;
+        this.capacidad = capacidad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getFrecuancia() {
+        return frecuancia;
+    }
+
+    public void setFrecuancia(int frecuancia) {
+        this.frecuancia = frecuancia;
+    }
+
+    @Override
+    public String toString() {
+        return "Ram{" +
+                "marca='" + marca + '\'' +
+                ", capacidad=" + capacidad +
+                ", frecuancia=" + frecuancia +
+                '}';
+    }
+}
